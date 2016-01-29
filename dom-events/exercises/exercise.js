3df04f188c6596f8955aa9dcc5634dac21126c1a
sleopardo <santiagoleopardo@gmail.com>
2016-01-29 12:37:45 -0300

    ejercicios de promise

--- dom-events/exercises/exercise.js
+++ dom-events/exercises/exercise.js
@@ -3,3 +3,17 @@
 //     of the personality list
 // 2 - the browser must only redirect to the twitter handle if it is clicked
 //      on the personality selected (not on the list)
+
+
+var list = document.getElementById('personalities'),
+	rightContainer = document.getElementById('personalitySelected');
+
+
+list.addEventListener('click', function(event) {
+	var target = event.target;
+	if (target.tagName === 'A' && target.className !== "selected") {
+		target.className = 'selected';
+		rightContainer.innerHTML = 'El chabon seleccionado es ' + target.parentNode.previousElementSibling.innerHTML;
+		event.preventDefault();
+	}
+});
